bc85e5213808be9d90d7b55c1307fa00640f41c9
BienHooFluss <jhjiang@163.com>
2022-04-10 11:41:47 +0800

    TaoQuick

---
+++ examples/TaoQuickShow/taoMacro.h
@@ -0,0 +1,7 @@
+#pragma once
+#define TaoQuickImport "file:///C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/src"
+#define TaoQuickImage "file:///C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/src/TaoQuick/Images/"
+#define qmlPath "file:///C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/examples/TaoQuickShow/Qml/"
+#define contentsPath "file:///C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/examples/TaoQuickShow/Contents/"
+#define imgPath "file:///C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/examples/TaoQuickShow/Image/"
+#define transDir "C:/Users/14732/Downloads/TaoQuick-master/TaoQuick-master/examples/TaoQuickShow/Trans/"
